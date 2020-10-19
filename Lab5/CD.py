dcc2e78e274d13963e02d357afe6167ae7848e32
Colin McKay <colin.mckay@carleton.ca>
2020-10-18 21:29:21 -0600

    Member D completed

--- Lab5/CD.py
+++ Lab5/CD.py
@@ -2,8 +2,10 @@
 #     The file to be jointly edited by Team Members C and D
 
 from C import *     # Line is added by Team Member C
+from D import *     # Line is added by Team Member D
 
 def function_CD():
     function_C()    # Line is added by Team Member C
+    function_D()    # Line is added by Team Member D
     print ("CD")
     
