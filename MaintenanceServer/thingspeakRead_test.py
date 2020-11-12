1dbe012bea2598603bddc8f723a4ffb0e4a3280d
Yisheng Li <yisheng.li@carleton.ca>
2020-11-11 23:15:48 -0500

    Added function of selecting WS to read bin levels for reading test & Added options of write to selected WS in wrting test

--- MaintenanceServer/thingspeakRead_test.py
+++ MaintenanceServer/thingspeakRead_test.py
@@ -4,24 +4,33 @@ import threading
 import json
 
 
-def read_level(channelID):
-    URL="https://api.thingspeak.com/channels/" + str(channelID) + "/feeds.json?api_key="
-    KEY='MG9FWWZOG8M0PCGK'
+stationNumber2channelID_key = {1: ['1222563','MG9FWWZOG8M0PCGK'], 2: ['1222564','TICC7ZFTEDZOPZ4F'], 3: ['1222565','A6YJ6ETTQ38WWHBN']}
+
+
+def get_level(stationNumber):
+    URL="https://api.thingspeak.com/channels/" + stationNumber2channelID_key[stationNumber][0] + "/feeds.json?api_key="
+    KEY=stationNumber2channelID_key[stationNumber][1]
     HEADER='&results=1'
     NEW_URL=URL+KEY+HEADER
-    print(NEW_URL)
-
     get_data=requests.get(NEW_URL).json()
 
     feeds=get_data['feeds'][0]
-    
-    print(feeds)
+    print("Bin1:" + feeds["field2"])
+    print("Bin2:" + feeds["field3"])
+    print("Bin3:" + feeds["field4"])
 
-    print(feeds["field2"])
-    print(feeds["field3"])
-    print(feeds["field4"])
-    
+def start_test():
+    while True:
+        print("Which Station?")
+        inputNum = int(input())
+        if(inputNum == 0):
+            break
+        
+        print("Bin levels at Waste Station" + str(inputNum))
+        get_level(inputNum)
+            
+
     
 if __name__ == '__main__':
-    read_level(1222563)
+    start_test()
 
--- MaintenanceServer/thingspeakWrite_test.py
+++ MaintenanceServer/thingspeakWrite_test.py
@@ -1,23 +1,22 @@
 import http.client
 import urllib.parse
-from time import sleep
+import time
 
 key_WS_1 = "BLT9N7F99578BAAM"
-def update_levels(channel_key, level1, level2, level3):
-    while True:
-        params = urllib.parse.urlencode({'field2': level1,'field3': level2,'field4': level3,'key':channel_key })
-        headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
-        conn = http.client.HTTPConnection("api.thingspeak.com:80")
-        try:
-            conn.request("POST", "/update", params, headers) 
-            response = conn.getresponse()
-            print(response.status, response.reason)
-            conn.close()
-        except:
-            print("connection failed")
-        break
+key_WS_2 = "GDHEA7VGR18FXYSU"  
+key_WS_3 = "KBQ5HYT7A32U1TQK" 
 
-        sleep(2)
+def update_levels(channel_key, level1, level2, level3):
+    params = urllib.parse.urlencode({'field2': level1,'field3': level2,'field4': level3,'key':channel_key })
+    headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+    conn = http.client.HTTPConnection("api.thingspeak.com:80")
+    try:
+        conn.request("POST", "/update", params, headers) 
+        response = conn.getresponse()
+        print(response.status, response.reason)
+        conn.close()
+    except:
+        print("connection failed")
+       
 if __name__ == "__main__":
-    while True:
-        update_levels(key_WS_1, 1.1, 2.2, 3.3)
+    update_levels(key_WS_3, 7, 8, 9)
