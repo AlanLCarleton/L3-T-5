03ef9914955bacd71e53d4634398997ada54c3fb
Ben Bozec <benbozec1@gmail.com>
2020-12-02 13:41:52 -0500

    Integratd Sensor controls into UI

--- WasteStation/UICode/WSUserInterface.py
+++ WasteStation/UICode/WSUserInterface.py
@@ -1,46 +1,54 @@
+import sys
+sys.path.insert(1, '../SensorControls')
 import time
+import runStation
 
 #The value of binID corresponds to each of the three bins in the system.
-#1 = Garbage, 2 = Plastics, 3 = Papers
-def chooseBin(binID):
-	#The following is placeholder code until servo is integrated with UI
-	if(binID == 1):
-		print("Accessing Garbage bin")
-		time.sleep(2)
-		print("Garbage bin is open. Please begin depositing.")
-		time.sleep(2)	
-		return True
-	elif(binID == 2):
-		print("Accessing Plastics bin")
-		time.sleep(2)
-		print("Plastics bin is open. Please begin depositing.")
-		time.sleep(2)
-		return True
-	elif(binID == 3):
-		print("Accessing Paper bin")
-		time.sleep(2)
-		print("Paper bin is open. Please begin depositing.")
-		time.sleep(2)
-		return True
-	else:
-		print("Provided Bin ID is invalid")
-		return False
+#1 = Garbage, 2 = Plastics, 3 = Papers, 4 = Compost
+def chooseBin(binID, stationID):
+	#Dictionary containing the channel id and API key of each station
+	stationData = {
+		1 : {'channel_key' : 'MG9FWWZOG8M0PCGK', 'channel_id': '1222563'},
+		2 : {'channel_key' : 'TICC7ZFTEDZOPZ4F', 'channel_id': '1222564'},
+		3 : {'channel_key' : 'A6YJ6ETTQ38WWHBN', 'channel_id': '1222565'}
+	}
+	result = activateStation(stationData[stationID]['channel_key'], binID)
+	
+	#Bin did not succesfully open
+	if(result == 0):
+		print("Something went wrong. Please try again.")
+		time.sleep(3)
+	#Bin successfully opens
+	elif(result == 1):
+		print("Please begin depositing")
+		time.sleep(5)
+	#Selected bin is full
+	elif(result == 2):
+		print("This bin is currently full")
+		for x in stationData:
+			if(x != stationID):
+				if(getEmptyBinLocation(stationData[stationID]['channel_key'], stationData[stationID]['channel_id'], binID)):
+					print("This bin is currently available at Waste Station" + str(x))
+			
 
-#returns bin location if bin level is below 100, else it returns 
+#returns True if bin level is below 100, else it returns False
 def getEmptyBinLocation(channel_key, channel_id, bin_id):
 	if(not isinstance(bin_id, int)):# Checks that bin_id is an int
 		return False
-	if(bin_id < 1 or bin_id > 3): # Only 1 to 3 are valid bin IDs
+	if(bin_id < 1 or bin_id > 4): # Only 1 to 4 are valid bin IDs
 		return False	
 
-	URL="https://api.thingspeak.com/channels/" + str(channel_id) + "/feeds.json?api_key=" 
+	URL="https://api.thingspeak.com/channels/" + channel_id + "/feeds.json?api_key=" 
 	KEY=channel_key
 	HEADER='&results=1'
 	NEW_URL=URL+KEY+HEADER
-
-	get_data=requests.get(NEW_URL).json()
 	
-	feeds = get_data['feeds'][0]
+	try:
+		station_data=requests.get(NEW_URL).json()
+	except:
+		print("Failure: No response")
+
+	feeds = station_data['feeds'][0]
 
 	#The bin_id value must be incremented by 1 because of the offset from the id and the corresponding Thingspeak field
 	field_id = bin_id + 1
@@ -55,7 +63,7 @@ def getEmptyBinLocation(channel_key, channel_id, bin_id):
 
 
 if __name__ == '__main__':
-	
+
 	#Each item follows the format of "Item name":"Bin ID"
 	itemList = {
 		"Bottle" : 2,
@@ -78,8 +86,10 @@ if __name__ == '__main__':
 			print("(1) for Garbage")
 			print("(2) for Plastic")
 			print("(3) for Paper")
+			print("(0) to go back")
 			binID = int(input())
-			chooseBin(binID)
+			if(bin_id != 0):
+				chooseBin(binID, stationID)
 
 		#Determine bin by searching for item
 		elif(option == 2):
@@ -87,10 +97,13 @@ if __name__ == '__main__':
 				print("Select an item:")
 				for x in itemList: # Display all of the items in the list
 					print(x)
+				print("Enter 'exit' to go back")
 				itemName = str(input())
 
 				if itemName in itemList:
-					chooseBin(itemList[itemName])
+					chooseBin(itemList[itemName], stationID)
+					break;
+				elif(itemName == "exit"):
 					break;
 				else:
 					print("That is an invalid input")
